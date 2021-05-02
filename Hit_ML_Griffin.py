591fbfd6994c7e9fe41e63e71324b412ea71ba1e
Griffin <griffin.miller0@gmail.com>
2021-05-02 00:36:53 -0700

    first commit

--- Hit_ML_Griffin.py
+++ Hit_ML_Griffin.py
@@ -1,13 +1,18 @@
 """
 Griffin Miller
-This program develops a machine learning model to predict if a ball put in
-play off of a given pitcher will result in a hit or an out.
+This program develops machine learning models to predict if a ball put in
+play off of a given pitcher will result in a hit or an out. It also evaluates
+each models ROC-AUC score and plots the curve.
 """
+from matplotlib import pyplot as plt
 from pybaseball import statcast_pitcher
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 # Tyler Glasnow MLBAM ID is 607192
 # Yu Darvish MLBAM ID is 506433
@@ -46,12 +51,11 @@ def clean(df):
     return df
 
 
-def create_model(df):
+def split_data(df):
     """
-    Takes in the clean and finalized dataframe. Trains a model to predict
-    whether or not a ball put into the field of play will result in a hit
-    or an out. The model is trainined on the given features and labels in
-    the training set and the models accuracy is measured on the test set.
+    Takes in the clean and finalized dataframe. Splits the data into
+    test and training sets and scales the features. returns the different
+    sets of data.
     """
     # drop any instances that have missing values
     df = df.dropna()
@@ -71,29 +75,115 @@ def create_model(df):
     features_train, features_test, label_train, label_test = \
         train_test_split(features, label, test_size=0.3)
 
+    standard = StandardScaler()
+
+    features_train = standard.fit_transform(features_train)
+    features_test = standard.transform(features_test)
+
+    return features_train, features_test, label_train, label_test
+
+
+def create_tree(f_train, f_test, l_train, l_test):
+    """
+    Creates a decision tree model and measures its accuracies.
+    returns the model object.
+    """
     # initialize model
     model = DecisionTreeClassifier(max_depth=2)
 
     # train it on training data
-    model.fit(features_train, label_train)
+    model.fit(f_train, l_train)
 
     # gather the model's predictions for train
-    train_predictions = model.predict(features_train)
+    train_predictions = model.predict(f_train)
 
     # gather the model's predictions for test
-    test_predictions = model.predict(features_test)
+    test_predictions = model.predict(f_test)
 
-    # calculate accuarcy of train
-    print('Accuracy: ', accuracy_score(label_train, train_predictions))
+    # calculate accuaracy of train
+    print('Tree Train Accuracy: ', accuracy_score(l_train, train_predictions))
 
     # calculate accuracy of test
-    print('Accuracy: ', accuracy_score(label_test, test_predictions))
+    print('Tree Test Accuracy: ', accuracy_score(l_test, test_predictions))
+
+    return model
+
+
+def create_regression(f_train, f_test, l_train, l_test):
+    """
+    Creates a logistic regression model and measures its accuracies.
+    returns the model object.
+    """
+    model = LogisticRegression()
+
+    # train it on training data
+    model.fit(f_train, l_train)
+
+    # gather the model's predictions for train
+    train_predictions = model.predict(f_train)
+
+    # gather the model's predictions for test
+    test_predictions = model.predict(f_test)
+
+    # calculate accuaracy of train
+    print('Reg Train Accuracy: ', accuracy_score(l_train, train_predictions))
+
+    # calculate accuracy of test
+    print('Reg Test Accuracy: ', accuracy_score(l_test, test_predictions))
+
+    return model
+
+
+def plot_roc_auc(tree, reg, f_test, l_test):
+    """
+    This function takes in both ML models as well as the test set's
+    features and labels. It calculates the probabilites of each instance
+    in each model and then calculates its AUC score. Additionally, it plots
+    the ROC curve of each model to compare visually.
+    """
+
+    # Calculate probabilites for each model
+    s_probs = [0 for _ in range(len(l_test))]
+    tree_probs = tree.predict_proba(f_test)
+    reg_probs = reg.predict_proba(f_test)
+
+    # Only keep postive outcomes
+    tree_probs = tree_probs[:, 1]
+    reg_probs = reg_probs[:, 1]
+
+    # Calculate AUC value for each model and 0.5
+    s_auc = roc_auc_score(l_test, s_probs)
+    tree_auc = roc_auc_score(l_test, tree_probs)
+    reg_auc = roc_auc_score(l_test, reg_probs)
+
+    print(f'Random prediction AUC: {s_auc:.3f}')
+    print(f'Decision Tree AUC: {tree_auc:.3f}')
+    print(f'Logistic Regression AUC: {reg_auc:3f}')
+
+    # calculate FPR & TPRs
+    s_fpr, s_tpr, s_thresh = roc_curve(l_test, s_probs)
+    tree_fpr, tree_tpr, tree_thresh = roc_curve(l_test, tree_probs)
+    reg_fpr, reg_tpr, reg_thresh = roc_curve(l_test, reg_probs)
+
+    # plot the curves
+    plt.plot(s_fpr, s_tpr, linestyle='--', label='Random Prediction')
+    plt.plot(tree_fpr, tree_tpr, linestyle='solid', label='Decision Tree AUC')
+    plt.plot(reg_fpr, reg_tpr, linestyle='dotted', label='Regression AUC')
+
+    plt.title('ROC Plot')
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.legend()
+    plt.savefig('Baseball_AUC.png')
 
 
 def main():
     data = get_player_stats(506433)
     clean_data = clean(data)
-    create_model(clean_data)
+    f_train, f_test, l_train, l_test = split_data(clean_data)
+    tree = create_tree(f_train, f_test, l_train, l_test)
+    reg = create_regression(f_train, f_test, l_train, l_test)
+    plot_roc_auc(tree, reg, f_test, l_test)
 
 
 if __name__ == '__main__':
